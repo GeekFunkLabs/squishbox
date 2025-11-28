b95496eddf412713cca1735a5e0401dcaba4735f
Bill Peterson <albedozero@gmail.com>
2025-11-28 11:27:24 -0600

    modify alsa_midi import

--- src/squishbox/midi.py
+++ src/squishbox/midi.py
@@ -1,54 +1,42 @@
 from threading import Thread
 
-from alsa_midi import SequencerClient, EventType, ALSAError, SYSTEM_ANNOUNCE, WRITE_PORT
+import alsa_midi
 
 from .config import CONFIG
 
 
 def midi_ports(**kwargs):
     return {f"{p.client_name.strip()}:{p.port_id}({p.name.strip()})": p
-            for p in sbclient.list_ports(**kwargs)}
+        for p in sbclient.list_ports(**{"type": alsa_midi.PortType.ANY} | kwargs)}
 
 
 def midi_connect():
     """Make MIDI connections as enumerated in config"""
     conn = set(CONFIG.get("midi_connections", []))
-    for i, iport in midi_ports(input=True).items():
-        for o, oport in midi_ports(output=True).items():
-            if {f"{i}>{o}", f"any>{o}", f"{i}>any"} & conn:
+    for src, sport in midi_ports(input=True).items():
+        for dest, dport in midi_ports(output=True).items():
+            if {f"{src}>{dest}", f"any>{dest}", f"{src}>any"} & conn:
                 try:
-                    sbclient.subscribe_port(iport, oport)
-                except ALSAError:
+                    sbclient.subscribe_port(sport, dport)
+                except alsa_midi.ALSAError:
                     pass
             else:
                 try:
-                    sbclient.unsubscribe_port(iport, oport)
-                except ALSAError:
+                    sbclient.unsubscribe_port(sport, dport)
+                except alsa_midi.ALSAError:
                     pass
-
-
-def add_rule(rule):
-    pass
-    
-    
-def clear_rules():
-    pass
-
-
-def send_event():
-    pass
 
 
 def process_events():
     while listening:
         evt = sbclient.event_input()
-        if evt.type == EventType.PORT_START:
+        if evt.type == alsa_midi.EventType.PORT_START:
             midi_connect()
 
 
-sbclient = SequencerClient("SquishBox")
-sbport = sbclient.create_port("SquishBox MIDI in", caps=WRITE_PORT)
-sbport.connect_from(SYSTEM_ANNOUNCE)
+sbclient = alsa_midi.SequencerClient("SquishBox")
+sbport = sbclient.create_port("SquishBox MIDI in", caps=alsa_midi.WRITE_PORT)
+sbport.connect_from(alsa_midi.SYSTEM_ANNOUNCE)
 
 listening = True
 Thread(target=process_events, daemon=True).start()
