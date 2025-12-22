dcf1bf892079dffffb380259b7129fd953486bf3
Bill Peterson <albedozero@gmail.com>
2025-12-21 22:24:46 -0600

    create script exception fallback

--- src/squishbox/scripts/launcher.py
+++ src/squishbox/scripts/launcher.py
@@ -16,76 +16,71 @@ def run_script(path):
     mod = module_from_spec(spec)
     CONFIG["startup_script"] = path.stem
     save_state(CONFIG_PATH, CONFIG)
-
-    spec.loader.exec_module(mod)
-    if hasattr(mod, "main"):
-        mod.main()
-
-    del CONFIG["startup_script"]
-    save_state(CONFIG_PATH, CONFIG)
-
-
-def main():
-    scripts = []
-    builtin_dir = Path(__file__).parent
-    for path in builtin_dir.glob("*.py"):
-        if path.stem in {"__init__", Path(__file__).stem}:
-            continue
-        scripts.append(path)
-    for d in CONFIG.get("script_paths", []):
-        d = Path(d).expanduser()
-        if not d.exists():
-            continue
-        for path in d.glob("*.py"):
-            scripts.append(path)
-    scripts.sort(key=lambda path: path.stem)
-    names = [path.stem for path in scripts]
-    if CONFIG.get("startup_script") in names:
-        run_script(scripts[
-            names.index(CONFIG["startup_script"])
-        ])
-
-    sb = SquishBox()
-    def clear_binds():
+    sb.knob1.clear_binds()
+    sb.button1.clear_binds()
+    
+    try:
+        spec.loader.exec_module(mod)
+        if hasattr(mod, "main"):
+            mod.main()
+    except Exception as e:
+        sb.display_error(e)
+    finally:
+        CONFIG.pop("startup_script", None)
+        save_state(CONFIG_PATH, CONFIG)
         sb.knob1.clear_binds()
         sb.button1.clear_binds()
-    def add_binds():
         sb.knob1.bind('left', sb.action_dec)
         sb.knob1.bind('right', sb.action_inc)
         sb.button1.bind('tap', sb.action_do)
         sb.button1.bind('hold', sb.action_back)
-    clear_binds()
-    add_binds()
-
-    last = 0
-    while True:
-        sb.lcd.clear()
-        sb.lcd.write(f"SquishBox {__version__}", row=0)
-        match sb.menu_choose([
-            *names,
-            "LCD Settings..",
-            "MIDI Settings..",
-            "WiFi Settings..",
-            "Exit"
-        ], row=ROWS - 1, i=last, timeout=0):
-            case last, "LCD Settings..":
-                sb.menu_lcdsettings()
-            case last, "MIDI Settings..":
-                sb.menu_midisettings()
-            case last, "WiFi Settings..":
-                sb.menu_wifisettings()
-            case last, "Exit" | None:
-                if sb.menu_exit() == "shell":
-                    break
-            case last, name:
-                sb.lcd.write(name.ljust(COLS), row=ROWS - 2)
-                sb.lcd.write("starting ".rjust(COLS), row=ROWS - 1)
-                clear_binds()
-                run_script(scripts[names.index(name)])
-                clear_binds()
-                add_binds()
 
 
-if __name__ == "__main__":
-    main()
+sb = SquishBox()
+sb.knob1.bind('left', sb.action_dec)
+sb.knob1.bind('right', sb.action_inc)
+sb.button1.bind('tap', sb.action_do)
+sb.button1.bind('hold', sb.action_back)
+
+paths = []
+builtin_dir = Path(__file__).parent
+for path in builtin_dir.glob("*.py"):
+    if path.stem in {"__init__", Path(__file__).stem}:
+        continue
+    paths.append(path)
+for d in CONFIG.get("script_paths", []):
+    d = Path(d).expanduser()
+    if not d.exists():
+        continue
+    for path in d.glob("*.py"):
+        paths.append(path)
+paths.sort(key=lambda path: path.stem)
+scripts = {path.stem: path for path in paths}
+if CONFIG.get("startup_script") in scripts:
+    run_script(scripts[CONFIG["startup_script"]])
+
+last = 0
+while True:
+    sb.lcd.clear()
+    sb.lcd.write(f"SquishBox {__version__}", row=0)
+    match sb.menu_choose([
+        *scripts,
+        "LCD Settings..",
+        "MIDI Settings..",
+        "WiFi Settings..",
+        "Exit"
+    ], row=ROWS - 1, i=last, timeout=0):
+        case last, "LCD Settings..":
+            sb.menu_lcdsettings()
+        case last, "MIDI Settings..":
+            sb.menu_midisettings()
+        case last, "WiFi Settings..":
+            sb.menu_wifisettings()
+        case last, "Exit" | None:
+            if sb.menu_exit() == "shell":
+                break
+        case last, name:
+            sb.lcd.write(name.ljust(COLS), row=ROWS - 2)
+            sb.lcd.write("starting..".rjust(COLS), row=ROWS - 1)
+            run_script(scripts[name])
 
